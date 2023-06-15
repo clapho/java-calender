28d5d64659a05f787cac7a0841593e87d522b50a
SoohoPark <clapp301@gmail.com>
2023-06-15 18:47:56 +0900

    특정 달의 최대 일수 출력하기

--- src/Calender.java
+++ src/Calender.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class Calender {
-    public static void main(String[] args) {
+    private static final int[] MAX_DAYS = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+    public int getMaxDaysOfMonth(int month) {
+        return MAX_DAYS[month-1];
+    }
+
+    public void printSampleCalender() {
         System.out.println("  일 월 화 수 목 금 토");
         System.out.println("-------------------");
         for(int i = 1; i <= 28; i++) {
@@ -8,5 +16,15 @@ public class Calender {
                 System.out.println('\n');
             }
         }
+    }
+
+    public static void main(String[] args) {
+        Calender cal = new Calender();
+        System.out.println("달을 입력하세요.");
+        Scanner s = new Scanner(System.in);
+        int month = s.nextInt();
+
+        System.out.printf("%d월은 %d일까지 있습니다.\n", month, cal.getMaxDaysOfMonth(month));
+        s.close();
     }
 }
