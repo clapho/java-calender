c7915adbe13bd8feee7da2fc67dbf8b1aee4798e
SoohoPark <clapp301@gmail.com>
2023-06-15 18:11:35 +0900

    캘린더 출력하기

--- src/Calender.java
+++ src/Calender.java
@@ -1,5 +1,12 @@
 public class Calender {
     public static void main(String[] args) {
-        System.out.println("Hello, Calender!");
+        System.out.println("  일 월 화 수 목 금 토");
+        System.out.println("-------------------");
+        for(int i = 1; i <= 28; i++) {
+            System.out.printf("%3d", i);
+            if(i % 7 == 0) {
+                System.out.println('\n');
+            }
+        }
     }
 }
