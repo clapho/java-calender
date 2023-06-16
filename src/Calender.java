a793eec16734c74fae467a021db8292f324be680
SoohoPark <clapp301@gmail.com>
2023-06-16 17:02:46 +0900

    달력찍기- 요일 입력받아 출력하기

--- src/Calender.java
+++ src/Calender.java
@@ -1,8 +1,9 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Calender {
     private static final int[] MAX_DAYS = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
     private static final int[] LEAF_MAX_DAYS = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    private static final String[] WEEK_DAYS = {"SU","MO", "TU", "WE", "TH", "FR", "SA"};
 
     public boolean isLeafYear(int year) {
         if(year % 4 == 0 && (year % 100 != 0 || year % 400 == 0))
@@ -19,18 +20,26 @@ public class Calender {
         }
     }
 
-    public void printCalender(int year, int month) {
+    public void printCalender(int year, int month, String weekDay) {
         System.out.printf("    <<%4d년%3d월>>\n", year, month);
-        System.out.println("  일 월 화 수 목 금 토");
-        System.out.println("-------------------");
+        System.out.println("  SU MO TU WE TH FR SA");
+        System.out.println("------------------------");
 
         int maxDay = getMaxDaysOfMonth(year, month);
+        int idx = Arrays.asList(WEEK_DAYS).indexOf(weekDay) + 1;
 
-        for(int i = 1; i <= maxDay; i++) {
-            System.out.printf("%3d", i);
+        int i = 1;
+
+        while(i <= maxDay + idx - 1) {
+            if(i < idx) {
+                System.out.printf("   ");
+            }else {
+                System.out.printf("%3d", i - idx + 1);
+            }
             if(i % 7 == 0) {
                 System.out.println();
             }
+            i++;
         }
         System.out.println();
     }
--- src/Prompt.java
+++ src/Prompt.java
@@ -6,22 +6,26 @@ public class Prompt {
         Calender cal = new Calender();
 
         while(true) {
-            System.out.println("연도를 입력하세요.");
+            System.out.println("연도를 입력하세요.(exit: -1)");
             System.out.print("YEAR> ");
             int year = s.nextInt();
+            if(year == -1) break;
+
             System.out.println("달을 입력하세요.");
             System.out.print("MONTH> ");
             int month = s.nextInt();
+            s.nextLine();
 
-            if(month == -1) {
-                break;
-            }
+            System.out.println("첫번째 요일을 입력하세요. (SU, MO, TU, WE, TH, FR, SA)");
+            System.out.print("WEEKDAY> ");
+            String weekDay = s.nextLine();
 
             if(month > 12 || month < 1) {
+                System.out.println("잘못된 입력입니다.");
                 continue;
             }
 
-            cal.printCalender(year, month);
+            cal.printCalender(year, month, weekDay);
         }
 
         System.out.println("Have a nice day!");
