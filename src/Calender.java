a24a10bcc829e0ea2c1f524fbd6fa3ce736ff12e
SoohoPark <clapp301@gmail.com>
2023-06-16 21:14:37 +0900

    달력찍기 - 요일 입력없이 자동계산

--- src/Calender.java
+++ src/Calender.java
@@ -3,7 +3,6 @@ import java.util.*;
 public class Calender {
     private static final int[] MAX_DAYS = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
     private static final int[] LEAF_MAX_DAYS = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-    private static final String[] WEEK_DAYS = {"SU","MO", "TU", "WE", "TH", "FR", "SA"};
 
     public boolean isLeafYear(int year) {
         if(year % 4 == 0 && (year % 100 != 0 || year % 400 == 0))
@@ -20,23 +19,32 @@ public class Calender {
         }
     }
 
-    public void printCalender(int year, int month, String weekDay) {
+    public int getTotalDays(int year, int month) {
+        int yearDays = (year-1) * 365 + (int)((year-1)/4) - (int)((year-1)/100) + (int)((year-1)/400);
+        int monthDays = 0;
+        for(int i = 1; i < month; i++) {
+            monthDays += getMaxDaysOfMonth(year, i);
+        }
+
+        return yearDays + monthDays; // (+1) 1일기준 인덱스
+    }
+
+    public void printCalender(int year, int month) {
         System.out.printf("    <<%4d년%3d월>>\n", year, month);
         System.out.println("  SU MO TU WE TH FR SA");
         System.out.println("------------------------");
 
         int maxDay = getMaxDaysOfMonth(year, month);
-        int idx = Arrays.asList(WEEK_DAYS).indexOf(weekDay) + 1;
+        int idx = getTotalDays(year, month) % 7;
+        int i = 0;
 
-        int i = 1;
-
-        while(i <= maxDay + idx - 1) {
-            if(i < idx) {
+        while(i <= maxDay + idx) {
+            if(i <= idx) {
                 System.out.printf("   ");
             }else {
-                System.out.printf("%3d", i - idx + 1);
+                System.out.printf("%3d", i - idx);
             }
-            if(i % 7 == 0) {
+            if((i+1) % 7 == 0) {
                 System.out.println();
             }
             i++;
--- src/Prompt.java
+++ src/Prompt.java
@@ -16,16 +16,13 @@ public class Prompt {
             int month = s.nextInt();
             s.nextLine();
 
-            System.out.println("첫번째 요일을 입력하세요. (SU, MO, TU, WE, TH, FR, SA)");
-            System.out.print("WEEKDAY> ");
-            String weekDay = s.nextLine();
 
             if(month > 12 || month < 1) {
                 System.out.println("잘못된 입력입니다.");
                 continue;
             }
 
-            cal.printCalender(year, month, weekDay);
+            cal.printCalender(year, month);
         }
 
         System.out.println("Have a nice day!");
